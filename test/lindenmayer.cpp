8396e18a0efaf7a5dff5f5c6496db03a1e0bfd93
XThexLonexWolfX <71769542+XThexLonexWolfX@users.noreply.github.com>
2022-12-15 13:57:15 +0100

    Added checks for throws so they dont just get thrown in the terminal

--- test/lindenmayer.cpp
+++ test/lindenmayer.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <catch2/catch.hpp>
+#include <iostream>
 #include "lindenmayer-system/lindenmayer.hpp"
 
 // Test isValid function
@@ -20,7 +21,7 @@ TEST_CASE("isValid, string", "[isValid] [string]")
 TEST_CASE("isValid, int", "[isValid] [int]")
 {
     const Production<int> production1({1}, {1, 2});
-    const Production<int> production2({1, 2}, {1, 2, 3});;
+    const Production<int> production2({1, 2}, {1, 2, 3});
     const std::unordered_set<int> alphabet = {1, 2};
     bool isValid1 = production1.isValidProduction(production1, alphabet);
     bool isValid2 = production2.isValidProduction(production2, alphabet);
@@ -30,12 +31,12 @@ TEST_CASE("isValid, int", "[isValid] [int]")
 
 
 // Test throws
-/*TEST_CASE("Interpreter, string, wrong production", "[interpreter] [string] [wrong production]") {
+TEST_CASE("Interpreter, string, wrong production", "[interpreter] [string] [wrong production]") {
     const Production<std::string> production({"a", "w", "q"}, {"a", "b", "c"});
     const std::unordered_set<std::string> alphabet = {"a", "b"};
     const std::unordered_set<Production<std::string>, custom_hash<std::string>> productions = {production};
     const std::vector<std::string> axiom = {"a"};
-    LSystemInterpreter<std::string> (axiom, productions, alphabet);
+    CHECK_THROWS_WITH(LSystemInterpreter<std::string> (axiom, productions, alphabet), "Production contains symbols not declared in the alphabet, or the predecessor contains more than one symbol!");
 }
 
 TEST_CASE("Interpreter, string, unique predecessor check", "[interpreter] [string] [unique predecessor check]") {
@@ -44,7 +45,7 @@ TEST_CASE("Interpreter, string, unique predecessor check", "[interpreter] [strin
     const std::unordered_set<std::string> alphabet = {"a", "b"};
     const std::unordered_set<Production<std::string>, custom_hash<std::string>> productions = {production1, production2};
     const std::vector<std::string> axiom = {"a"};
-    LSystemInterpreter<std::string> (axiom, productions, alphabet);
+    CHECK_THROWS_WITH(LSystemInterpreter<std::string> (axiom, productions, alphabet), "Some productions do not have a unique predecessor");
 }
 
 TEST_CASE("Interpreter, string, alphabet check", "[interpreter] [string] [alphabet check]") {
@@ -53,8 +54,8 @@ TEST_CASE("Interpreter, string, alphabet check", "[interpreter] [string] [alphab
     const std::unordered_set<std::string> alphabet = {"a", "b", "c"};
     const std::unordered_set<Production<std::string>, custom_hash<std::string>> productions = {production1, production2};
     const std::vector<std::string> axiom = {"a"};
-    LSystemInterpreter<std::string> (axiom, productions, alphabet);
-}*/
+    CHECK_THROWS_WITH(LSystemInterpreter<std::string> (axiom, productions, alphabet), "Not each symbol in the alphabet has a production");
+}
 
 // Test generate function
 TEST_CASE("Interpreter, string, single alphabet check", "[interpreter] [string] [single alphabet check]") {
@@ -116,6 +117,25 @@ TEST_CASE("Interpreter, int, two alphabet check", "[interpreter] [int] [two alph
     std::vector<int> LSystem = LSystemConstructed.generate(3);
     std::vector<int> CheckVector = {1, 2, 2, 2, 2, 2, 2, 2};
     CHECK(LSystem == CheckVector);
+}
+
+// Test generate function
+TEST_CASE("Stringbuilder for python", "[python]") {
+    const Production<std::string> production1({"F"}, {"F", "[", "+", "F", "]", "-", "F"});
+    const Production<std::string> production2 ({"["}, {"["});
+    const Production<std::string> production3 ({"+"}, {"+"});
+    const Production<std::string> production4 ({"]"}, {"]"});
+    const Production<std::string> production5 ({"-"}, {"-"});
+    const std::unordered_set<std::string> alphabet = {"F", "+", "-", "[", "]"};
+    const std::vector<std::string> axiom = {"F"};
+    std::unordered_set<Production<std::string>, custom_hash<std::string>> productions = {production1, production2, production3, production4, production5};
+    LSystemInterpreter<std::string> LSystemConstructed(axiom, productions, alphabet);
+    std::vector<std::string> LSystem = LSystemConstructed.generate(4);
+    std::string TestString;
+    for (std::string& i : LSystem) {
+        TestString.append(i);
+    }
+    std::cout << TestString;
 }
 
 
